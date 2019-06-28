2cec0ba773da2c284d7e258b18c888c2546fea9c
Alice Brunel <alc.brunel@gmail.com>
2019-06-28 13:35:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,9 +4,9 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(array)
-  puts "#{array[0]}|#{array[2]}|#{array[3]}
+  puts " #{array[0]} | #{array[2]} | #{array[3]}
   -----------
-  #{array[4]}|#{array[5]}|#{array[6]}
+   #{array[4]} | #{array[5]} |#{array[6]} 
   -----------
-  #{array[7]}|#{array[8]}|#{array[9]}"
+   #{array[7]} | #{array[8]} | #{array[9]}"
 end
